9da8429f8f84da75dbf4ea4bd557b8a0065dc005
djcerdas <d2words@gmail.com>
2017-09-29 14:04:59 -0600

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -4,6 +4,7 @@
 # Version     	: 1.0
 # Copyright   	: GPLv2
 # Description	:  This script create a desire amount of files in an specific directory
+# To use it: ./cf.sh <the amount of file you will to be created> "<path where you will files to be created>"
 
 
 #!/bin/bash
