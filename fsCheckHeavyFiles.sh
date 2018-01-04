d980a75177eb76c1643e36d6a0b826a69343fe9c
djcerdas <d2words@gmail.com>
2018-01-04 16:13:40 -0600

    Replace ${size::-1}  variable and ./fs.sh name
    - Replace ${size::-1} with nsize variable to save the number of size variable,
       because presents an issue( substring expression < 0) with bash-4.1.2-48.el6.x86_64 in OVS 3.4.3
    - Replace ./fs.sh with  fsCheckHeavyFiles.sh , that is more descriptive

--- fsCheckHeavyFiles.sh
+++ fsCheckHeavyFiles.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Name        	: fs.sh
+# Name        	: fsCheckHeavyFiles.sh
 # Author      	: david.cerdas
-# Version     	: 1.0
+# Version     	: 1.01
 # Copyright   	: GPLv2
 # Description	: Script to list the Files that are using most of the space on determine FS
-# Usage		: ./fs.sh <FileSystem to analyze>  <Size><Unit M/G> <number of files otherwise 20 is the default>
+# Usage		: ./fsCheckHeavyFiles.sh <FileSystem to analyze>  <Size><Unit M/G> <number of files otherwise 20 is the default>
 
 
 fs=$1
 size=$2
+nsize=$(echo $size|sed 's/.$//')
 nfiles=$3
 [ ${nfiles:-null} = "null" ]&&export nfiles=20
 
@@ -17,9 +18,9 @@ nfiles=$3
 errormgs(){
 	clear
 	echo "Please verify the syntax and the values are not null/0"
-	echo "# ./fs.sh <FileSystem to analyze>  <Size><Unit M/G> <number of files otherwise 20 is the default>"
+	echo "# ./fsCheckHeavyFiles.sh <FileSystem to analyze>  <Size><Unit M/G> <number of files otherwise 20 is the default>"
 	echo -e "\nExample:"
-	echo "# ./fs.sh /var/log  2G 25"
+	echo "# ./fsCheckHeavyFiles.sh /var/log  2G 25"
 }
 
 # If fs is not null, then run the find depending on the unit specified
@@ -27,10 +28,10 @@ if [ "${fs:-null}" != "null" ];then
 	unit="${size:$((${#size}-1)):1}"
 	case $unit in
 		m|M)
-			find $fs -xdev -size +${size::-1}M -type f -exec ls -la {} \; 2>/dev/null |sort -n -k 5 |tail -$nfiles
+			find $fs -xdev -size +${nsize}M -type f -exec ls -la {} \; 2>/dev/null |sort -n -k 5 |tail -$nfiles
 			;;
 		g|G)
-			find $fs -xdev -size +${size::-1}G -type f -exec ls -la {} \; 2>/dev/null |sort -n -k 5 |tail -$nfiles
+			find $fs -xdev -size +${nsize}G -type f -exec ls -la {} \; 2>/dev/null |sort -n -k 5 |tail -$nfiles
 			;;
 		*)
 			errormgs
